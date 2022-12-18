ce4965bef7376da0f5c82c71cefdc82fdc6ae4fb
Putut Yusri Bahtiar <64458989+pututyb@users.noreply.github.com>
2022-12-18 18:44:16 +0700

    Initial Commit

---
+++ BuyGame/BuyGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuyGameApp.swift
+//  BuyGame
+//
+//  Created by Putut Yusri Bahtiar on 18/12/22.
+//
+
+import SwiftUI
+
+@main
+struct BuyGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
